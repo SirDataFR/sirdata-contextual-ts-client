442d6d01cc1aa84ab1dc86758b616a849aefc0ab
Rémi Demol <momolrd@gmail.com>
2019-12-09 11:53:16 +0100

    rebuild & add missing files

---
+++ lib/cache.d.ts
@@ -0,0 +1,3 @@
+import { PageCategorizationResponse } from "./models/page_categorization_response";
+export declare function SetCategorizeUrlCache(url: string, categorized: {}): void;
+export declare function GetCategorizeFromCache(url: string): PageCategorizationResponse;
