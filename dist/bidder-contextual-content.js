1753b418865dcd3ba9812254232f499929c8f88d
Remi Demol <rde@sirdata.fr>
2025-08-07 11:54:18 +0200

    bidder : update path & endpoint

--- dist/bidder-contextual-content.js
+++ dist/bidder-contextual-content.js
@@ -40,7 +40,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var http_1 = require("./http");
 var contextual_1 = require("./rest/contextual");
 var page_categorization_response_1 = require("./models/page_categorization_response");
-exports.apiUrl = "https://contextual-content.sirdata.io/api/v1/public";
+exports.apiUrl = "https://contextual-content.sirdata.io/api/v1";
 var objectPerToken = new Map();
 var cacheUrlCategorized = new Map();
 function NewContextual(token) {
@@ -665,7 +665,7 @@ var RestContextual = /** @class */ (function (_super) {
             });
         });
     };
-    RestContextual.contextualPath = '/contextual';
+    RestContextual.contextualPath = '/content';
     return RestContextual;
 }(rest_1.Rest));
 exports.RestContextual = RestContextual;
